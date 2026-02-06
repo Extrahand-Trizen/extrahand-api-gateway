3fac424ec26f7d884285dc13719c1f932020dae4
Tarun516 <taruncv516@gmail.com>
2026-02-06 08:11:26 +0530

    fix: type fix

--- src/services/baseService.ts
+++ src/services/baseService.ts
@@ -148,7 +148,7 @@ export abstract class BaseService {
         'Authorization': `Bearer ${tokenString}`,
         'X-User-Id': userToken.uid, // Keep for backward compatibility
         ...(userToken.profileId && {
-          'X-Profile-Id': typeof userToken.profileId === 'string' ? userToken.profileId : userToken.profileId.toString(),
+          'X-Profile-Id': userToken.profileId,
         }),
         ...(userToken.sessionId && { 'X-Session-Id': userToken.sessionId }),
       };
